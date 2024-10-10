81863ede29cabe6126d9ac8115c090185975db34
Camilla Barros <camillaerbarros@gmail.com>
2024-10-10 10:20:19 -0300

    configurando as cores que serão usadas no projeto no tailwind.config.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="roboto-medium">Camilla Barros</h1>
+      <h1 className="roboto-medium text-mint">Camilla Barros</h1>
       <h1 className="quicksand-light">izabelinhaa</h1>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,66 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        lightCyan: {
+          light: "#E5F0F0",
+          DEFAULT: "#CFE4E4",
+          dark: "#CBE2E2",
+        },
+        myrtleGreen: {
+          light: "#4B9595",
+          DEFAULT: "#418080",
+          dark: "#3E7A7A",
+        },
+        darkCyan: {
+          light: "#6AA0A0",
+          DEFAULT: "#5D9292",
+          dark: "#588989",
+        },
+        lihtBlue: {
+          light: "#A6C9C9",
+          DEFAULT: "#97BFBF",
+          dark: "#8DB9B9",
+        },
+        tuftsBlue: {
+          light: "#2894E2",
+          DEFAULT: "#1E8CD9",
+          dark: "#1B7EC5",
+        },
+        auburn: {
+          light: "#AF3140",
+          DEFAULT: "#9E2C39",
+          dark: "#8F2834",
+        },
+        redCrayola: {
+          light: "#ED5A6B",
+          DEFAULT: "#EA3E51",
+          dark: "#E9354A",
+        },
+        grape: {
+          light: "#953cb9",
+          DEFAULT: "#8635A5",
+          dark: "#7d329a",
+        },
+        charcoal: {
+          light: "#394C56",
+          DEFAULT: "#2F3E46",
+          dark: "#293C3D",
+        },
+        lapisLazuli: {
+          light: "#1B679D",
+          DEFAULT: "#165683",
+          dark: "#15507A",
+        },
+        gamboge: {
+          light: "#F1A841",
+          DEFAULT: "#EF9D2D",
+          dark: "#EE961B",
+        },
+        mint: {
+          light: "#8BD0AD",
+          DEFAULT: "#74C69D",
+          dark: "#6EC499",
+        },
       },
     },
   },
