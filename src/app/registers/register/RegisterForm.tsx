18103d92b38c9686b8f44a06a8f45ff2304e2bc4
MrGalva07 <lucas.galvao0707@gmail.com>
2024-10-26 21:21:32 -0300

    registerInterfaceFirstC

--- src/app/registers/register/RegisterForm.tsx
+++ src/app/registers/register/RegisterForm.tsx
@@ -2,7 +2,51 @@ import React from 'react'
 
 const RegisterForm = () => {
   return (
-    <div>RegisterForm</div>
+    <main>
+      <form>
+          <div className='flex'>
+            <div>
+            <label htmlFor="name">Nome do tutor:</label>
+            <input type="text" />
+            </div>
+
+            <div>
+            <label htmlFor="cpf">CPF do tutor:</label>
+            <input type="text"/>
+            </div>
+          </div>
+
+
+          <div>
+            <div>
+          <label htmlFor="telephone">Telefone do tutor:</label>
+          <input type="text"/>
+          </div>
+          <div>
+            <label htmlFor="enedereço">Endereço do tutor:</label>
+            <input type="text"/>
+            </div>
+          </div>
+
+
+          <div>
+          <div>
+            <label htmlFor="email">E-mail do tutor:</label>
+            <input type="email"/>
+            </div>
+            <div>
+            <label htmlFor="telephone">Telefone do tutor:</label>
+            <input type="text"/>
+            </div>
+          </div>
+
+
+         
+      </form>
+
+
+
+    </main>
   )
 }
 
---
+++ src/app/registers/register/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import RegisterForm from './RegisterForm'
+
+const page = () => {
+  return (
+    <section>
+        <RegisterForm/>
+    </section>
+  )
+}
+
+export default page
