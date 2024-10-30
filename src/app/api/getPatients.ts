f0f62038428d5bad89fe9e6a002c8e65cd8e8a35
Camilla Barros <camillaerbarros@gmail.com>
2024-10-30 11:05:36 -0300

    ajustando fetch da api

--- src/app/api/getPatients.ts
+++ src/app/api/getPatients.ts
@@ -2,7 +2,7 @@ export const fetchPatients = async () => {
   try {
     // const response = await fetch("/api/patients"); // Atualizando a URL para a API interna
     const response = await fetch("https://pi-t1-gp2-clinica.onrender.com/pets"); // Atualizando a URL para a API interna
-    const response = await fetch("/api/patients"); // Atualizando a URL para a API interna
+    // const response = await fetch("/api/patients"); // Atualizando a URL para a API interna
     // const response = await fetch("https://pi-t1-gp2-clinica.onrender.com/pets"); // Atualizando a URL para a API interna
     if (!response.ok) {
       throw new Error("Erro ao buscar dados");
