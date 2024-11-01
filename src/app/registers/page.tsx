8235da23b27f1b599f51c642d655dc415edaba7c
MrGalva07 <lucas.galvao0707@gmail.com>
2024-11-01 11:16:09 -0300

    commit first register

--- src/app/registers/page.tsx
+++ src/app/registers/page.tsx
@@ -1,5 +1,8 @@
+
+import Link from "next/link";
 import { Header } from "../components/navigationScreen/header/header";
 import { SideBar } from "../components/navigationScreen/sidebar/sidebar";
+import { BtnColorBg } from "../components/ui/btn/btnColorBg";
 
 export default function Registration() {
   return (
@@ -8,6 +11,16 @@ export default function Registration() {
       <SideBar />
       <div>
         <h1>Você está em Cadastros</h1>;
+        <p>Este é o conteúdo da página de cadastros</p>;
+      
+      <div className="m-96">
+        
+          <Link href="/registers/register" >
+          <BtnColorBg content="Novo cadastro" />
+          </Link>
+          </div>
+       
+        
       </div>
     </>
   );
--- src/app/registers/register/RegisterForm.tsx
+++ src/app/registers/register/RegisterForm.tsx
@@ -1,43 +1,48 @@
+import { Header } from '@/app/components/navigationScreen/header/header'
+import { SideBar } from '@/app/components/navigationScreen/sidebar/sidebar'
+import { BtnRecover } from '@/app/components/ui/btn/BtnRecover'
 import React from 'react'
 
 const RegisterForm = () => {
   return (
-    <main>
-      <form>
-          <div className='flex'>
-            <div>
-            <label htmlFor="name">Nome do tutor:</label>
-            <input type="text" />
+    <div className='flex justify-center text-left'>
+      <Header/>
+      <SideBar/>
+      
+      <h1 className='flex justify-start text-start fixed items-center m-64 text-4xl left-0'>Cadastro</h1>
+      <main className='flex justify-center flex-col fixed items-center top-36 left-0 text-start w-full h-full'>
+      <form className=' flex justify-center left-0 flex-col gap-5 input-lg'>
+          <div className='flex gap-4'>
+            <div className='flex flex-col'>
+            <label htmlFor="name" className='text-xl font-semibold'>Nome do tutor:</label>
+            <input type="text" className="input input-bordered  h-3/4 pt-2 pb-2 text-lg "  placeholder='Digite o nome completo'  />
             </div>
 
-            <div>
-            <label htmlFor="cpf">CPF do tutor:</label>
-            <input type="text"/>
-            </div>
-          </div>
-
-
-          <div>
-            <div>
-          <label htmlFor="telephone">Telefone do tutor:</label>
-          <input type="text"/>
-          </div>
-          <div>
-            <label htmlFor="enedereço">Endereço do tutor:</label>
-            <input type="text"/>
+            <div className='flex flex-col'>
+            <label htmlFor="cpf" className='text-xl font-semibold'>CPF do tutor:</label>
+            <input type="text" className="input input-bordered  h-3/4 pt-2 pb-2 text-lg " placeholder='000.000.000-00'/>
             </div>
           </div>
 
 
-          <div>
-          <div>
-            <label htmlFor="email">E-mail do tutor:</label>
-            <input type="email"/>
+          <div className='flex gap-4'>
+            <div className='flex flex-col'>
+          <label htmlFor="telephone" className='text-xl font-semibold'>Telefone do tutor:</label>
+          <input type="text" className="input input-bordered  h-3/4 pt-2 pb-2 text-lg "   placeholder='(00) 0 0000-0000'/>
+          </div>
+          <div className='flex flex-col'>
+            <label htmlFor="enedereço" className='text-xl font-semibold'>Endereço do tutor:</label>
+            <input type="text" className="input input-bordered  h-3/4 pt-2 pb-2 text-lg " placeholder='Rua,avenida,etc.'  />
             </div>
-            <div>
-            <label htmlFor="telephone">Telefone do tutor:</label>
-            <input type="text"/>
+          </div>
+
+
+          <div className='flex gap-4'>
+          <div className='flex flex-col'>
+            <label htmlFor="email" className='text-xl font-semibold'>E-mail do tutor:</label>
+            <input type="email" className="input input-bordered  h-3/4 pt-2 pb-2 text-lg "placeholder='exemplo@gmail.com'/>
             </div>
+           
           </div>
 
 
@@ -47,6 +52,7 @@ const RegisterForm = () => {
 
 
     </main>
+    </div>
   )
 }
 
