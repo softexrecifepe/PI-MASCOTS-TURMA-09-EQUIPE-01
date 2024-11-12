f2dc24aca2704cdb95762c3b5148d30cd8193d18
Camilla Barros <camillaerbarros@gmail.com>
2024-11-12 17:50:43 -0300

    feat: popula internamento comdados do json

--- src/app/admission/page.tsx
+++ src/app/admission/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import data from "../../../public/components/data.json";
 import { Header } from "../components/navigationScreen/header/header";
 import { SideBar } from "../components/navigationScreen/sidebar/sidebar";
 import { BreadCrumb } from "../components/ui/breadcrumbs/breadcrumb";
@@ -26,27 +26,37 @@ export default function Admission() {
             <div className="flex flex-col gap-5">
               <div className="py-5 px-5 flex flex-col gap-10 border shadow-md text-2xl roboto-regular">
                 <h2 className="">Pacientes internados</h2>
-                <div className="flex flex-row flex-wrap gap-5">
-                  <CardTypeAdmission
-                    pet_name="Bolinha"
-                    breed="SRD"
-                    weight="15kg"
-                    owners_cpf="000000"
-                    boxLocation="G-01"
-                    color_classification="bg-green-500"
-                    category="Menos grave"
-                    profilePic="https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?q=80&w=1443&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                  />
-                  <CardTypeAdmission
-                    pet_name="Bolinha"
-                    breed="SRD"
-                    weight="15kg"
-                    owners_cpf="000000"
-                    boxLocation="G-01"
-                    color_classification="bg-blue-500"
-                    category="Caso leve"
-                    profilePic="https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?q=80&w=1443&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                  />
+                {/* <div className="flex flex-row flex-wrap gap-5">
+                  {data.map((item) => (
+                    <CardTypeAdmission
+                      key={item.owners_cpf}
+                      pet_name={item.name}
+                      breed={item.breed}
+                      weight={item.weight}
+                      discharge={item.exitDate}
+                      owners_cpf={item.owners_cpf}
+                      boxLocation={item.boxLocation}
+                      color_classification={item.color_classification}
+                      category={item.category}
+                      profilePic="https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?q=80&w=1443&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    />
+                  ))}
+                </div> */}
+                <div className="grid gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                  {data.map((item) => (
+                    <CardTypeAdmission
+                      key={item.owners_cpf}
+                      pet_name={item.name}
+                      breed={item.breed}
+                      weight={item.weight}
+                      discharge={item.exitDate}
+                      owners_cpf={item.owners_cpf}
+                      boxLocation={item.boxLocation}
+                      color_classification={item.color_classification}
+                      category={item.category}
+                      profilePic="https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?q=80&w=1443&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    />
+                  ))}
                 </div>
               </div>
               <div className="py-5 px-5 flex flex-col gap-10 border shadow-md text-2xl roboto-regular">
--- src/app/components/ui/card/cardTypeAdmission.tsx
+++ src/app/components/ui/card/cardTypeAdmission.tsx
@@ -1,12 +1,13 @@
 import { Avatar } from "@mui/material";
 import { FaInfoCircle } from "react-icons/fa";
+import Tooltip from "@mui/material/Tooltip";
 
 type InformationProps = {
   pet_name: string;
   owners_cpf: string;
   breed: string;
   weight: string;
-  discharge?: Date;
+  discharge?: string;
   boxLocation: string;
   profilePic: string;
   color_classification: string;
@@ -30,22 +31,29 @@ export default function CardTypeAdmission({
         <div
           className={`flex flex-row px-2 py-2 rounded-t-lg ${color_classification}`}
         >
-          <span className="text-sm text-white">{category}</span>
+          <span className="text-sm text-white roboto-bold">{category}</span>
         </div>
         <div className="flex flex-row py-5 px-5 gap-10">
           <div className="flex flex-col gap-1">
-            <span className="text-lg roboto-regular">
-              {pet_name} <span className="text-gray-400">({owners_cpf})</span>
-            </span>
-            <span className="text-xs text-gray-400"></span>
+            <span className="text-lg roboto-regular">{pet_name} </span>
+            <span className="text-gray-400 text-sm">{owners_cpf}</span>
             <span className="text-xs text-gray-400">
               {breed}, {weight}
+            </span>
+            <span className="text-xs text-gray-400">
+              {discharge
+                ? `Previsão de alta ${discharge}`
+                : "Sem previsão de alta"}
             </span>
             <span className="text-sm">{boxLocation}</span>
           </div>
           <div className="flex flex-col gap-3 justify-between items-center">
             <Avatar src={profilePic}></Avatar>
-            <FaInfoCircle size={15} />
+            <Tooltip title="Adicionar ocorrência" arrow>
+              <button>
+                <FaInfoCircle size={15} />
+              </button>
+            </Tooltip>
           </div>
         </div>
       </div>
